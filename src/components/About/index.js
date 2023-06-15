988f10004d8a5418e4470430c78ea43a7e847c34
Nathan Shown <nshown@instructors.2u.com>
2023-06-15 12:04:36 -0400

    Updated home page wording, about to reflect current resume, and styling to lower text in mobile view.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,7 +5,7 @@ function About() {
   return (
     <section className='about'>
       <div>
-        <p>I am passionate  about software engineering.  I love the challenge of creating a new application or enhancing an existing application to better serve the userbase.  My appreciation of application engineering is rooted in my love of problem solving and providing users with a new, enjoyable, and useful experience.  I have worked as an engineer for over a decade and I continue to delight in the experience.</p>
+        <p>I am extremely enthusiastic about software engineering!  I love the challenge of creating a new application or enhancing an existing application to better serve clients.  My appreciation of software development is rooted in my love of problem solving and desire to provide clients with new, enjoyable, and useful solutions to achieve their business goals.  I have worked as an engineer for over a decade and I continue to delight in the experience.</p>
       </div>
       <div>
         <h1>Skills</h1>
@@ -14,25 +14,25 @@ function About() {
         </div>
       </div>
       <div>
-        <h1>Experience</h1>
+        <h1>Work History</h1>
         <div>
           <div>
             <h3>Lead Software Engineer and CEO skyLyn Applications LLC</h3>
             <h4>skyLyn Applications LLC</h4>
             <h5>2014 - present</h5>
-            <p>Creating a social network web app focusing on users sharing customizable adventure experiences coded using Node, Typescript, Sequelize, and React.  Using C#, JavaScript, SQL, and HTML5 to produce a web scraping desktop app called ScrapeMate.  Using PHP, SQL, and JavaScript to produce the website to distribute ScrapeMate, receive/track user orders, and educate my userbase.</p>
+            <p>Creating a social network web application focusing on users sharing customizable adventure experiences coded using Node, Typescript, and Sequelize for the backend and React for the frontend.  Using C#, JavaScript, SQL, and HTML5 to produce a web scraping desktop application called ScrapeMate.  Using PHP, SQL, and JavaScript to produce the website to distribute ScrapeMate, receive/track user orders, and educate the userbase.</p>
           </div>
           <div>
             <h3>Data Visualization and Web Development Bootcamp Instructor</h3>
             <h4>edX</h4>
             <h5>2020 - present</h5>
-            <p>Instructing students with limited technical backgrounds in Python, Pandas, Matplotlib, Flask, ML, HTML, JavaScript, Node, SQL, React, and more with the intention they become competent entry level web engineers or data analyst.  Creating additional curriculum materials to assist students learning.  One-on-one tutoring students requiring additional assistance.</p>
+            <p>Instructing students with limited technical backgrounds in Python, Pandas, Matplotlib, Flask, ML, HTML, JavaScript, Node, SQL, React, and more to prepare them to become competent web engineers or data analysts.  Creating additional curriculum materials to assist student learning.  One-on-one tutoring of students requiring additional assistance.</p>
           </div>
           <div>
             <h3>Lead Software Engineer</h3>
-            <h4>Experis</h4>
+            <h4>International Monetary Fund</h4>
             <h5>2010 - 2014</h5>
-            <p>Used C#, VB, XAML, SQL, and other technologies in Visual Studio to produce Excel addins and standalone applications to store, manipulate, organized and visualize data.  Developed informational materials and hosted instructional sessions to educate users in the proper use of our applications.  Gathered, designed and prioritized project requirements.</p>
+            <p>Used C#, VB, XAML, SQL, and other technologies in Visual Studio to produce Excel addins and standalone applications to store, manipulate, organized and visualize data.  Developed informational materials and hosted seminars to educate users in the proper use of our applications.  Gathered, designed and prioritized project requirements.</p>
           </div>
         </div>
       </div>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,7 +5,7 @@ function Home() {
     <section>
       <h1 className='home-title'><span style={{fontSize: 'xx-large'}}>Hello, </span><br /><span style={{fontSize: 'xx-large'}}>I'm</span> Nathan Shown!</h1>
       <p>
-        I'm a full stack software engineer dedicated to <br className='hideable'/>tackling tricky problems and making great web apps
+        I'm a full stack software engineer dedicated to <br className='hideable'/>taking on big challenges and making great web apps
       </p>
     </section>
   );
